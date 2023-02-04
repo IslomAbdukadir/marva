9604b4ffee7c22f594c5e9556b6cf845c84e0fa2
islomabduqodir <islomabduqodir@gmail.com>
2023-02-04 23:01:38 +0500

    awdawd

--- js/script.js
+++ js/script.js
@@ -453,3 +453,28 @@ var mask = new IMask(element3, maskOptions);
 var mask = new IMask(element4, maskOptions);
 
 // 
+
+//change the integers below to match the height of your upper div, which I called
+//banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
+//to figure out what the scroll position is when exactly you want to fix the nav
+//bar or div or whatever.  I stuck in the console.log for you.  Just remove when
+//you know the position.
+var headerHeight = $("header.hero").outerHeight();
+
+$(window).scroll(function () {
+  console.log("scrollTop: " + $(window).scrollTop());
+  console.log("headerHeight: " + headerHeight);
+
+  if ($(window).scrollTop() > headerHeight) {
+    $("#about").addClass("animate__animated");
+    $("#about").addClass("animate__slideInUp");
+  }
+  if ($(window).scrollTop() > headerHeight + 800) {
+    $("#course").addClass("animate__animated");
+    $("#course").addClass("animate__slideInUp");
+  }
+  if ($(window).scrollTop() > headerHeight + 1400) {
+    $("#feature").addClass("animate__animated");
+    $("#feature").addClass("animate__slideInUp");
+  }
+});
